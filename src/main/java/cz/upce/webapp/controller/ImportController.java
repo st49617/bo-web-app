9acea033bda88aea75ffde639013b4bed978c5dd
Pavel Jetensky <pavel.jetensky@hooyu.com>
2019-11-17 12:58:01 +0100

    search remembered

--- src/main/java/cz/upce/webapp/controller/ImportController.java
+++ src/main/java/cz/upce/webapp/controller/ImportController.java
@@ -30,10 +30,7 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -79,6 +76,11 @@ public class ImportController
     @Autowired
     private StoredFileStorageService storedFileService;
 
+    @GetMapping("/order")
+    public String order()
+    {
+        return "cart/email";
+    }
     @PostMapping("/upload")
     public String singleFileUpload(@RequestParam("file") MultipartFile file, @RequestParam("supplierId") Integer supplierId, RedirectAttributes redirectAttributes)
     {
