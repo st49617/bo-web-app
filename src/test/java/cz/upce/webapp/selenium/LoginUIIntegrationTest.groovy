3614b52c2f6dd499116fc7f6eddbc7f5e2eac6f7
Pavel Jetensky <pavel.jetensky@hooyu.com>
2019-11-27 12:15:01 +0100

    testing of sending e-mail

--- src/test/java/cz/upce/webapp/selenium/LoginUIIntegrationTest.groovy
+++ src/test/java/cz/upce/webapp/selenium/LoginUIIntegrationTest.groovy
@@ -32,7 +32,7 @@ public class LoginUIIntegrationTest extends Specification
     {
         given:
             def password = "\$2a\$12" + "\$TNH.f4YLpJgCVduQPUejUOuIifkJ5T7DjINwgslGI5s4u7.Plm6d."
-            creator.save(new User(email: "tester2@test.cz", password: password))
+            creator.save(new User(email: "tester25@test.cz", password: password))
         when:
             loginPage
                     .visit()
